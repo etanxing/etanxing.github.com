4073291872028d4f2072727029460bc98ca3bc27
William Xing <william@workswell.com.au>
2019-01-02 19:37:32 +1030

    Dynamic index page updated

---
+++ adoptapet/script.js
@@ -0,0 +1,45 @@
+const root = document.getElementById("root");
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+const id = getParameterByName('p')
+
+fetch(
+  `https://gv5wp4nyl5.execute-api.ap-southeast-2.amazonaws.com/dev/getpets/${id}`
+)
+  .then(resp => resp.json())
+  .then(data => {
+    document.title = `${data.type} in ${
+      data.location
+    } | Workswell - Frontend Development Specialist`;
+    root.innerHTML = `<h1>Hey, we found following pets in ${data.location}</h1>
+	<span><i>Note: Some pets may be unavailable after <time>${
+    data.dt
+  }</time></i></span>
+  <ol id="petlist">
+  ${data.pets.map((pet, index) => {
+    const img = pet.img
+      ? `<p><a href="https://www.adoptapet.com.au/pet/${
+          pet.id
+        }"><img src="https://www.adoptapet.com.au/${
+          pet.img
+        }" style="width: 100%; max-width: 300px;"></a></p>`
+      : "";
+    return `${index + 1}. <b>${pet.name}</b> - ${pet.sex}, ${pet.age}, ${
+      pet.breed
+    } ${img}
+  <p><a href="https://www.adoptapet.com.au/pet/${
+    pet.id
+  }" class="button">More details</a></p>
+</li>`;
+  })}
+  </ol>`;
+  });
